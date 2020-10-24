6241de44667b8163c4743251aafa9fb9853d2181
blackorbit1 <enzo.dutra@gmail.com>
2020-10-24 21:09:09 +0200

    ajout commentaires du prof au TD3

--- main.ml
+++ main.ml
@@ -29,3 +29,21 @@ type I =
     | A of { fpos : I ; apos : I }
 *)
 
+(* === === Visionnage de la video du TD3 de romain === === *)
+
+(* Commentaires de romain pendant le TD *)
+M ::= x   --> VarOfString
+λx.M      --> AbsOfString * lterme
+M M       --> AppOf (lterme) * (lterme)
+
+(* 
+Pour éviter de faire "qqch de syntaxique" cad changer
+les noms de variables à la voler pour pas que 2 variables
+différentes aient le meme nom, on peut regarder les indices 
+de Debruijn 
+*)
+
+(*
+partie Sémentique (optionnelle), pour évaluer, on fait de la 
+gauche vers la droite (voir à 34:30 sur la video du TD)
+*)
