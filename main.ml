3fd5406b9113389663279b5d5bc326a4527adf9b
blackorbit1 <enzo.dutra@gmail.com>
2020-10-27 13:15:46 +0100

    1e partie du projet

--- main.ml
+++ main.ml
@@ -8,33 +8,52 @@ DUTRA Enzo
 *)
 
 
-
-type lterme =
-    Vari of string
-  | Oper of string
-  | Val of int
-  | Fpos of *lterme
-  | Apos of *lterme
-  | Corps of *lterme 
-(*  | Elements of lterme list *)
+(* 
+l = "lambda"
+V = value
+L = lambda
+A = application 
+*)
+type l =
+      V of string
+    | L of { vari : string ; corps : l }
+    | A of { fpos : l ; apos : l }
 ;;
 
+(* l -> string *)
+let rec print_lterme lterme = match lterme with
+  | V v -> v
+  | L {vari = v; corps = c} -> "λ" ^ v ^ "." ^ (print_lterme c)
+  | A {fpos = f; apos = a} -> "(" ^ (print_lterme f) ^ " " ^ (print_lterme a) ^ ")"
+;;
+  
+(* string -> l *)
+let cvar str = V str ;;
 
-(* type prof *)
+(* string, l -> l *)
+let clam str lambda = L { vari = str ; corps = lambda } ;;
 
-(*
-type I =
-    V of string
-    | L of { vari : string ; corps : l }
-    | A of { fpos : I ; apos : I }
-*)
+(* l, l -> l *)
+let capp lambda1 lambda2 = A { fpos = lambda1 ; apos = lambda2 } ;;
+
+(* Exemples *)
+let v1 = cvar "sd" ;;
+let v2 = cvar "hy" ;;
+let l1 = clam "az" v1 ;;
+let a1 = capp l1 v2 ;;
+
+print_lterme v1 ;;
+print_lterme l1 ;;
+print_lterme a1 ;;
+
 
 (* === === Visionnage de la video du TD3 de romain === === *)
 
-(* Commentaires de romain pendant le TD *)
+(* Commentaires de romain pendant le TD 
 M ::= x   --> VarOfString
 λx.M      --> AbsOfString * lterme
 M M       --> AppOf (lterme) * (lterme)
+*)
 
 (* 
 Pour éviter de faire "qqch de syntaxique" cad changer
