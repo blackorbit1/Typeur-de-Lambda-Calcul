07b4c2a92876e0d0e094107def84e6421e545a8c
blackorbit1 <enzo.dutra@gmail.com>
2020-10-31 19:54:03 +0100

    debut beta reduction

--- main.ml
+++ main.ml
@@ -12,7 +12,7 @@ DUTRA Enzo
 
 
 (* 
-l = "lambda"
+l = "lambda terme"
 V = value
 L = lambda
 A = application 
@@ -31,13 +31,13 @@ let rec print_lterme lterme = match lterme with
 ;;
   
 (* string -> l *)
-let cvar str = V str ;;
+let cvar str = (V str : l) ;;
 
 (* string, l -> l *)
-let clam str lambda = L { vari = str ; corps = lambda } ;;
+let clam str lambda = (L { vari = str ; corps = lambda } : l) ;;
 
 (* l, l -> l *)
-let capp lambda1 lambda2 = A { fpos = lambda1 ; apos = lambda2 } ;;
+let capp lambda1 lambda2 = (A { fpos = lambda1 ; apos = lambda2 } : l) ;;
 
 (* === === === Exemples *)
 
@@ -120,18 +120,135 @@ print_lterme a2 ;;
 print_lterme (barendregt a2) ;;
 
 
+(* l -> string -> l -> l *)
+let rec instantie l x a = match l with
+  | V v -> if v = x then a else cvar v
+  | L {vari = v; corps = c} -> clam v (instantie c x a)
+  | A {fpos = f; apos = ap} -> capp (instantie f x a) (instantie ap x a)
+;;
+
+
+(* 
+er = "evaluer resultat"
+*)
+type er = content of { 
+  status :  string ; 
+  res :     l; 
+  rmem :    RempMap }
+;;
+
+let eval = ref 0 ;;
+
+(* l -> RempMap[string * string] -> l *)
+let ltrcbv_etape_rec l map = 
+;;
+
+(* l -> l *)
+let ltrcbv_etape l = 
+  let l_barendregt = (barendregt l) in
+  let map = RempMap.empty in
+  let c = 0 in
+  Format.printf "%s" l_barendregt ;
+  match !eval with
+    | 1000 -> Format.printf "%s" "*** STOP : Trop de réductions ***" ; eval := 0
+    | _ -> eval := !eval + 1 ; let nouveau = ltrcbv_rec l_barendregt map in match nouveau with
+      | content { status = "KO" } -> l
+      | content { res = r ; rmem = rm } -> Format.printf "→%s" (print_lterme r) ; 
+
+;;
+
+
+
+
+(*
+let ltrcbv_etape_acc l acc = 
+;;
+*)
+
+(* func ltrcbv_etape(l lterme) lterme *)
+
 (* === === === Exo 2.3 *)
+
+(* 
+s = syntaxe
+V = variable
+L = arguments
+A = resultat 
+*)
+type s =
+  V of string
+| A of { targ : s ; tres : s } 
+;;
+
+
+(* string -> s *)
+let cSvar str = (V str : s) ;;
+
+(* s, s -> s *)
+let cSapp stype1 stype2 = (A { targ = stype1 ; tres = stype2 } : s) ;;
+
+(* s -> string *)
+let rec print_syntax s = match s with
+  | V v -> v
+  | A { targ = a ; tres = r } -> "(" ^ (print_syntax a) ^ ") → " ^ (print_syntax r)
+;;
+
+(* === === === Exemples *)
+
+(*
+Test 1 : (A) → (B) → C
+*)
+
+let t1_var1 = cSvar "A" ;;
+let t1_var2 = cSvar "B" ;;
+let t1_var3 = cSvar "C" ;;
+
+let t1_app2 = cSapp t1_var2 t1_var3 ;;
+let t1_app1 = cSapp t1_var1 t1_app2 ;;
+
+print_syntax t1_app1 ;;
+
+(*
+Test 2 : ((A) -> B) -> C
+*)
+
+let t2_var1 = cSvar "A" ;;
+let t2_var2 = cSvar "B" ;;
+let t2_var3 = cSvar "C" ;;
+
+let t2_app1 = cSapp t2_var1 t2_var2 ;;
+let t2_app2 = cSapp t2_app1 t2_var3 ;;
+
+print_syntax t2_app2 ;;
+
+
+(* s -> s -> bool *)
+let rec stype_egal t1 t2 = match (t1, t2) with
+  | (V v, A a) -> false
+  | (A a, V v) -> false
+  | (V v1, V v2) -> v1 = v2
+  | (A { targ = a1 ; tres = r1 }, A { targ = a2 ; tres = r2 }) -> (stype_egal a1 a2) && (stype_egal r1 r2)
+;;
+
+(* === === === Exemples *)
+
+stype_egal t1_app1 t2_app2 ;; (* False *)
+stype_egal t1_app1 t1_app1 ;; (* True *)
+stype_egal t2_app2 t2_app2 ;; (* True *)
+
+stype_egal t2_var1 t2_var2 ;; (* False *)
+stype_egal t2_var1 t2_var1 ;; (* True *)
+
 
 
 
 (* === === Visionnage de la video du TD3 de romain === === *)
 
-(* Commentaires de romain pendant le TD 
-M ::= x   --> VarOfString
+(*
 λx.M      --> AbsOfString * lterme
 M M       --> AppOf (lterme) * (lterme)
 *)
-
+ 
 (* 
 Pour éviter de faire "qqch de syntaxique" cad changer
 les noms de variables à la voler pour pas que 2 variables
@@ -143,7 +260,6 @@ de Debruijn
 partie Sémentique (optionnelle), pour évaluer, on fait de la 
 gauche vers la droite (voir à 34:30 sur la video du TD)
 *)
-
 
 (*
 Parties demandées : 1 à 4
